080218cb5433888be40390046d0c39b5b89a5648
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2021-07-21 11:25:15 -0500

    Added an apply now modal with email address

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let menuBtn = document.querySelector('#mobile-menu-container');
 let classes = document.querySelector('#mobile-menu').classList;
 
+let modal = document.querySelector('#toggle-modal');
+let modalBackdrop = document.querySelector('#toggle-backdrop');
+let applyBtn = document.querySelector('#applyBtn');
+
 // console.log(menuBtn);
 
 menuBtn.addEventListener('click', (event) => {
@@ -8,4 +12,14 @@ menuBtn.addEventListener('click', (event) => {
     classes.toggle('block');
     console.log(classes);
 
+})
+
+applyBtn.addEventListener('click', (event) => {
+    // event.preventDefault();
+    modal.classList.toggle('hidden');
+})
+
+modalBackdrop.addEventListener('click', (event) => {
+    // event.preventDefault();
+    modal.classList.toggle('hidden');
 })
